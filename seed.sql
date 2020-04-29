ff2176ab84e897b249ac998b16fc15e28276eac2
Philip Hurst <youngphil9@gmail.com>
2020-04-29 08:15:28 -0500

    fixed seeds

--- seed.sql
+++ seed.sql
@@ -1,32 +1,32 @@
-INSERT INTO department (id, name)
-VALUES (1, 'Sales');
+INSERT INTO department (name)
+VALUES ('Sales');
 
-INSERT INTO department (id, name)
-VALUES (2, 'Finance');
+INSERT INTO department (name)
+VALUES ('Finance');
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES (1, 'Sales Lead', 100000, 1);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Sales Lead', 100000, 1);
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES (2, 'Entry Sales', 50000, 1);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Entry Sales', 50000, 1);
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES (3, 'Accountant', 80000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Accountant', 80000, 2);
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES (4, 'Financial Analyst', 70000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Financial Analyst', 70000, 2);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Phil', 'Hurst', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Phil', 'Hurst', 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Ken', 'Hurst', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Ken', 'Hurst', 1);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Schwyn', 'Francis', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Schwyn', 'Francis', 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Kasey', 'Hurst', 2, 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Kasey', 'Hurst', 2);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Joe', 'Schmoe', 2, 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Joe', 'Schmoe', 2);
